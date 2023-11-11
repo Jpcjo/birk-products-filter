6161a8393c697e5816be2682af2b3602a77b896d
Fay Wu <fwudesign@gmail.com>
2023-11-11 14:43:46 +1000

    Width update

--- src/components/SubWomen.jsx
+++ src/components/SubWomen.jsx
@@ -16,6 +16,7 @@ const SubWomen = () => {
   const { sidebarToggle } = useSelector((state) => state.dropdownState);
   const [isClicked, setIsClicked] = useState(false);
   // console.log("SubWomen", subWomenClicked);
+  console.log(isClicked);
 
   const clickToGoBack = () => {
     dispatch(isSubWomenClosed());
@@ -23,37 +24,41 @@ const SubWomen = () => {
   };
 
   return (
-    <ul className={`flex flex-col space-y-3 w-full`}>
-      {!subWomenClicked ? (
+    <ul
+      className={`flex flex-col space-y-3 w-full ${
+        !subWomenClicked ? " translate-x-full" : " translate-x-0"
+      } duration-500 ease-in-out transform`}
+    >
+      {/* {!subWomenClicked ? (
         <SidebarContent />
-      ) : (
-        <>
-          <button
-            className="flex pb-3 items-center  border-b "
-            onClick={clickToGoBack}
-          >
-            <RiArrowLeftSLine />
-            <div className=" font-semibold ">WOMEN</div>
-          </button>
+      ) : ( */}
+      <>
+        <button
+          className={`flex pb-3 items-center  border-b`}
+          onClick={clickToGoBack}
+        >
+          <RiArrowLeftSLine />
+          <div className=" font-semibold ">WOMEN</div>
+        </button>
 
-          {subSidebar.map((section, index) => {
-            return (
-              <div
-                className="flex flex-row justify-between items-start ml-4  border-b  "
-                key={index}
+        {subSidebar.map((section, index) => {
+          return (
+            <div
+              className="flex flex-row justify-between items-start ml-4  border-b  "
+              key={index}
+            >
+              <li
+                className={`text-black cursor-pointer hover:text-gray-400 pb-3 `}
+                //   onClick={() => handleMenuItemClick(section)}
               >
-                <li
-                  className={`text-black cursor-pointer hover:text-gray-400 pb-3 `}
-                  //   onClick={() => handleMenuItemClick(section)}
-                >
-                  {section}
-                </li>
-                <RiArrowRightSLine className="w-6 h-6 text-gray-200" />
-              </div>
-            );
-          })}
-        </>
-      )}
+                {section}
+              </li>
+              <RiArrowRightSLine className="w-6 h-6 text-gray-200" />
+            </div>
+          );
+        })}
+      </>
+      {/* )} */}
     </ul>
   );
 };
